5ef3a9296d2783176d7b4dff463f2286b6b6449c
Jacque Schrag <jnschrag@gmail.com>
2018-11-28 15:05:34 -0500

    Update array format for related content list

--- _plugins/related-content-list.rb
+++ _plugins/related-content-list.rb
@@ -2,7 +2,7 @@ Jekyll::Hooks.register :pages, :post_render do |post|
   # inject modification_time in post's datas.
   puts post.data
   if post.data['is_for_related_content_list']
-    post.data['posts_list'] = post.content
+    post.data['posts_list'] = post.content.split(",")
     puts post.data
   end
 end
