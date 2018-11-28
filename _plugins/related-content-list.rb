db50fa54491327e392e7657b87cce88fccb66b9c
Jacque Schrag <jnschrag@gmail.com>
2018-11-28 14:59:44 -0500

    Add plugin to dynamically update the front matter of the related content yml file

---
+++ _plugins/related-content-list.rb
@@ -0,0 +1,8 @@
+Jekyll::Hooks.register :pages, :post_render do |post|
+  # inject modification_time in post's datas.
+  puts post.data
+  if post.data['is_for_related_content_list']
+    post.data['posts_list'] = post.content
+    puts post.data
+  end
+end
