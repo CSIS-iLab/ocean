6779d7e053e479939da99400288e28689e48ea21
leezaweb <leeza.web@gmail.com>
2018-12-06 16:18:37 -0500

    revert header js

--- assets/_js/header.js
+++ assets/_js/header.js
@@ -4,7 +4,6 @@ const Header = () => {
   const minimal_class = 'site-header--minimal'
   let scrollPos = window.pageYOffset
   let offsetScroll = true
-  let lastScrollPos = 0
 
   window.addEventListener('scroll', function() {
     if (document.documentElement.classList.contains('js-no-scroll')) {
@@ -19,33 +18,12 @@ const Header = () => {
 
     if (scrollPos > header_height) {
       offsetScroll = false
+
       header.classList.add(minimal_class)
     } else {
       offsetScroll = true
       header.classList.remove(minimal_class)
-
-      if (scrollPos < lastScrollPos) {
-        const nav_trigger = document.querySelector('.site-header__nav-trigger')
-        const nav = document.querySelector('.site-header__nav-menu')
-
-        nav.setAttribute('aria-expanded', 'false')
-        nav.classList.remove('is-active')
-        nav_trigger.classList.remove('is-active')
-
-        const share_trigger = document.querySelector(
-          '.site-header__share-trigger'
-        )
-        const share = document.querySelector('.site-header__share ul.share')
-
-        if (share_trigger) {
-          share.setAttribute('aria-expanded', 'false')
-          share.classList.remove('is-active')
-          share_trigger.classList.remove('is-active')
-        }
-      }
     }
-
-    lastScrollPos = scrollPos
   })
 }
 
