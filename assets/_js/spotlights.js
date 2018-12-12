2c95aeeab19ceffe14b302b0ef7ad37e4166aac3
Anne Lee Steele <aleesteele@gmail.com>
2018-12-12 14:19:10 -0500

    ready for PR

--- assets/_js/spotlights.js
+++ assets/_js/spotlights.js
@@ -1,11 +1,9 @@
 import HeaderShare from './spotlights/header-share'
 import ProgressBar from './spotlights/progress-bar'
-import ResizeComponents from './spotlights/resize-components'
 import TableOfContents from './spotlights/tableofcontents'
 
 window.addEventListener('DOMContentLoaded', () => {
   HeaderShare()
   ProgressBar()
-  ResizeComponents()
   TableOfContents()
 })
