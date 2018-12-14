7a0f86edc3b3bee064c12dd048743edb715751ad
Jacque Schrag <jnschrag@gmail.com>
2018-12-14 15:47:38 -0500

    Remove resize component JS

--- assets/_js/spotlights.js
+++ assets/_js/spotlights.js
@@ -1,13 +1,11 @@
 import HeaderShare from './spotlights/header-share'
 import ProgressBar from './spotlights/progress-bar'
-import ResizeComponents from './spotlights/resize-components'
 import TableOfContents from './spotlights/tableofcontents'
 import Tippy from './spotlights/tippy'
 
 window.addEventListener('DOMContentLoaded', () => {
   HeaderShare()
   ProgressBar()
-  ResizeComponents()
   TableOfContents()
   Tippy()
 })
