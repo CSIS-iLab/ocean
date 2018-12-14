371515b25cb31c06ca375fe39857b9398120d880
Jacque Schrag <jnschrag@gmail.com>
2018-12-14 13:14:04 -0500

    Remove margin on search box, increase padding on cutout, add timer to cutout JS

--- assets/_js/archive-cutout.js
+++ assets/_js/archive-cutout.js
@@ -7,15 +7,19 @@ const ArchiveCutout = () => {
 
   const location = window.location.pathname.split('/').filter(f => f)
 
-  const cutoutHeight =
-    location[0] === 'authors'
-      ? Math.min(cutout.offsetHeight, 101)
-      : cutout.offsetHeight
+  // Wrap in timeout function to address bug in Chrome: See
+  // https://stackoverflow.com/questions/10268892/outerheighttrue-gives-wrong-value
+  setTimeout(function() {
+    const cutoutHeight =
+      location[0] === 'authors'
+        ? Math.min(cutout.offsetHeight, 101)
+        : cutout.offsetHeight
 
-  document.documentElement.style.setProperty(
-    '--archive-cutout-height',
-    cutoutHeight + 'px'
-  )
+    document.documentElement.style.setProperty(
+      '--archive-cutout-height',
+      cutoutHeight + 'px'
+    )
+  }, 1)
 }
 
 export default ArchiveCutout
