66ba6eeed84ef7756b660be44345af9e639bc990
Jacque Schrag <jnschrag@gmail.com>
2018-12-19 20:08:17 -0500

    Fix progress bar calculations

--- assets/_js/spotlights/progress-bar.js
+++ assets/_js/spotlights/progress-bar.js
@@ -3,13 +3,16 @@ const ProgressBar = () => {
   const b = document.body
   const st = 'scrollTop'
   const sh = 'scrollHeight'
+  const client_height =
+    h.clientHeight +
+    document.querySelector('.spotlight__footer').clientHeight +
+    document.querySelector('.site-footer').clientHeight +
+    100
   const header = document.querySelector('.site-header')
-  const content_height =
-    document.querySelector('.spotlight__content').clientHeight - 100
   let scroll
 
   document.addEventListener('scroll', function() {
-    scroll = ((h[st] || b[st]) / ((h[sh] || b[sh]) - content_height)) * 100
+    scroll = ((h[st] || b[st]) / ((h[sh] || b[sh]) - client_height)) * 100
     header.style.setProperty('--scroll-progress', scroll + '%')
   })
 }
