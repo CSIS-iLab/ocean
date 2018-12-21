e307b620d8379502b60f9d2898d912b82cc7c33d
Jacque Schrag <jnschrag@gmail.com>
2018-12-21 17:20:54 -0500

    Update video src for mobile

--- assets/_js/spotlights/scs/intro.js
+++ assets/_js/spotlights/scs/intro.js
@@ -1,25 +1,55 @@
+import Breakpoints from '../../breakpoints'
 import ScrollMagic from 'ScrollMagic'
 
 const Intro = () => {
   const graphicEl = document.querySelector('.scs__intro')
+  const videoEl = document.querySelector('.scs__intro-video')
+  const videoPlayer = videoEl.querySelector('.scs__intro-video-player')
+  const videoPlayerSource = videoPlayer.querySelector('source')
+  const contentEl = document.querySelector('.spotlight__content')
   const triggerEls = Array.from(
     graphicEl.querySelectorAll('.scs__intro-trigger')
   )
   const stepNum = triggerEls.length
-  // const penultimate = triggerEls[stepNum - 2]
   const ultimate = triggerEls[stepNum - 1]
+  let src = videoPlayer.getAttribute('data-src')
+
+  if (Breakpoints.isMobile()) {
+    src = videoPlayer.getAttribute('data-src-mobile')
+  }
+
+  // videoPlayerSource.src = src
+  // videoPlayer.load()
+  // videoPlayer.play()
+
+  setTimeout(() => {
+    videoPlayerSource.src = src
+    videoPlayer.load()
+    videoPlayer.play()
+  }, 500)
 
   // init controller
-  const controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onEnter'
-    }
-  })
+  const controller = new ScrollMagic.Controller()
 
   new ScrollMagic.Scene({
-    triggerElement: ultimate
+    triggerElement: ultimate,
+    triggerHook: 'onEnter'
   })
     .setClassToggle('.scs__intro-map', 'is-active')
+    .addTo(controller)
+
+  new ScrollMagic.Scene({
+    triggerElement: contentEl,
+    triggerHook: 'onEnter'
+  })
+    .on('enter', e => {
+      videoEl.classList.remove('is-active')
+      videoPlayer.pause()
+    })
+    .on('leave', e => {
+      videoEl.classList.add('is-active')
+      videoPlayer.play()
+    })
     .addTo(controller)
 }
 
