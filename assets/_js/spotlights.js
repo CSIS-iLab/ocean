93e69b9b6d35a059127ceab8fb32c2055c4473ee
leezaweb <leeza.web@gmail.com>
2019-01-08 11:30:34 -0500

    give background text scroll it's own import

--- assets/_js/spotlights.js
+++ assets/_js/spotlights.js
@@ -1,21 +1,21 @@
+import BackgroundTextScroll from './spotlights/backgroundTextScroll'
 import Explainer from './spotlights/explainer'
 import HeaderShare from './spotlights/header-share'
 import ImageGallery from './spotlights/image-gallery'
 import Lightbox from './spotlights/lightbox'
+import objectFitVideos from 'object-fit-videos'
 import ProgressBar from './spotlights/progress-bar'
-import Stickyfill from 'stickyfilljs'
 import TableOfContents from './spotlights/tableofcontents'
 import Tippy from './spotlights/tippy'
 
 window.addEventListener('DOMContentLoaded', () => {
+  BackgroundTextScroll()
   Explainer()
   HeaderShare()
   ImageGallery()
   Lightbox()
+  objectFitVideos()
   ProgressBar()
-  Stickyfill.add(
-    document.querySelector('.sc-background-text-scroll__background')
-  )
   TableOfContents()
   Tippy()
 })
