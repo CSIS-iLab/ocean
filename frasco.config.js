7e4fae9f2acaf334e47d54b93012f0abe0156374
Jacque Schrag <jnschrag@gmail.com>
2019-01-08 19:43:36 -0500

    Start to separate out webpack config to own file in prep for cache

--- frasco.config.js
+++ frasco.config.js
@@ -80,52 +80,5 @@ module.exports = {
       grid: 'autoplace',
       browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
     }
-  },
-
-  webpack: {
-    mode: 'production',
-    // devtool: 'source-map',
-    // plugins: [new BundleAnalyzerPlugin()],
-    output: {
-      filename: chunkData => {
-        return chunkData.chunk.entryModule._identifier.includes('spotlights/')
-          ? 'spotlights/[name].js'
-          : '[name].js'
-      }
-    },
-    externals: {
-      algoliasearch: 'algoliasearch',
-      Flickity: 'flickity',
-      LuminousLightbox: 'luminous-lightbox',
-      'instantsearch.js/es': 'instantsearch',
-      'pixi.js': 'PIXI',
-      Plyr: 'plyr',
-      objectFitImages: 'object-fit-images',
-      objectFitVideos: 'object-fit-videos',
-      ScrollMagic: 'ScrollMagic',
-      SmoothScroll: 'smooth-scroll',
-      Stickyfill: 'stickyfilljs',
-      TimelineMax: 'TimelineMax',
-      'tippy.js': 'tippy',
-      TweenMax: 'TweenMax',
-      Highcharts: 'Highcharts'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-      ]
-    },
-    resolve: {
-      modules: ['node_modules'],
-      alias: {
-        TweenLite: 'gsap/src/minified/TweenLite.min.js',
-        TweenMax: 'gsap/src/minified/TweenMax.min.js',
-        TimelineLite: 'gsap/src/minified/TimelineLite.min.js',
-        TimelineMax: 'gsap/src/minified/TimelineMax.min.js',
-        ScrollMagic: 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
-        'animation.gsap':
-          'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js'
-      }
-    }
   }
 }
--- gulp_tasks/webpack.js
+++ gulp_tasks/webpack.js
@@ -6,16 +6,17 @@ const named = require('vinyl-named')
 const plumber = require('gulp-plumber')
 const webpackStream = require('webpack-stream')
 const webpack = require('webpack')
+const webpackConfig = require('../webpack.config.js')
 
 const entry = []
 for (var i = 0; i <= config.js.entry.length - 1; i++) {
   entry.push(config.assets + '/' + config.js.src + '/' + config.js.entry[i])
 }
 
-if (config.tasks.eslint) config.webpack.module.rules.push(config.eslintLoader)
+if (config.tasks.eslint) webpackConfig.module.rules.push(config.eslintLoader)
 
-config.webpack.watch = argv.watch
-config.webpack.mode = argv.mode || config.webpack.mode
+webpackConfig.watch = argv.watch
+webpackConfig.mode = argv.mode || webpackConfig.mode
 
 gulp.task('webpack', function() {
   return gulp
@@ -23,12 +24,12 @@ gulp.task('webpack', function() {
     .pipe(plumber())
     .pipe(named())
     .pipe(babel())
-    .pipe(webpackStream(config.webpack, webpack))
+    .pipe(webpackStream(webpackConfig, webpack))
     .pipe(gulp.dest(config.assets + '/' + config.js.dest))
 })
 
 // For internal use only
 gulp.task('_webpack', function() {
-  config.webpack.watch = config.tasks.watch
+  webpackConfig.watch = config.tasks.watch
   gulp.start('webpack')
 })
