a1c8645c1cee1299e8cbe51ecb339f10e5da361d
Jacque Schrag <jnschrag@gmail.com>
2019-03-14 18:45:42 -0400

    Add month selector & ice year filter interactivity

--- assets/_js/spotlights/arctic/scrollytelling-map.js
+++ assets/_js/spotlights/arctic/scrollytelling-map.js
@@ -1,17 +1,29 @@
-import Breakpoints from '../../breakpoints'
+import IceSelector from './scrollytelling-ice-selector'
+import Selector from './scrollytelling-selector'
 import ScrollMagic from 'ScrollMagic'
 import Stickyfill from 'stickyfilljs'
 
 const Map = () => {
+  IceSelector.setListener()
+  Selector.setListener()
   Stickyfill.add(document.querySelector('.arctic__map-map'))
 
   const graphicEl = document.querySelector('.arctic__map')
   const triggerEls = Array.from(
     graphicEl.querySelectorAll('.arctic__map-trigger')
   )
-  const labelEl = document.querySelector('.arctic__legend-curr')
+  const descEl = document.querySelector('.arctic__legend-desc')
+  const medianLegendEl = document.querySelector('.arctic__legend-key--median')
 
   const interval = 750
+  const text = {
+    s0: 'Watch the ice extent grow and diminish throughout 2018.',
+    s1:
+      'Choose a month from the dropdown above to view the ice extent on the map.',
+    s2: 'The ice extent reaches its maximum in late winter.',
+    s3: 'Click on labels below to toggle layers on map.',
+    s4: 'The ice extent reaches its minimum in late summer.'
+  }
 
   let monthsTimer
   let yearTimer
@@ -19,15 +31,16 @@ const Map = () => {
   const stepActions = {
     s0() {
       clearTimeout(monthsTimer)
-      document.getElementById('sc_jan18').classList.add('is-active')
-      labelEl.innerHTML = 'January'
+      Selector.setValue('jan')
       document.getElementById('sc_slide1').classList.remove('is-active')
+      descEl.innerHTML = text['s0']
     },
     s1() {
       document.getElementById('sc_slide1').classList.add('is-active')
-      document.getElementById('sc_dec18').classList.remove('is-active')
-      document.getElementById('sc_mar18').classList.remove('is-active')
+      descEl.innerHTML = text['s0']
       document.getElementById('sc_slide2').classList.remove('is-active')
+      medianLegendEl.classList.remove('is-active')
+
       const months = {
         jan: 'January',
         feb: 'February',
@@ -48,23 +61,17 @@ const Map = () => {
       let counter = 0
       monthsTimer = setTimeout(function tick() {
         let month = monthsShort[counter]
-        document.getElementById('sc_' + month + '18').classList.add('is-active')
-        labelEl.innerHTML = months[month]
-
-        if (counter > 0) {
-          const prevMonth = monthsShort[counter - 1]
-          document
-            .getElementById('sc_' + prevMonth + '18')
-            .classList.remove('is-active')
-        }
+        Selector.setValue(month)
 
         if (counter === monthsShort.length - 1) {
+          Selector.toggleDisabledStatus(false)
+          descEl.innerHTML = text['s1']
           clearTimeout(monthsTimer)
           return
         }
 
         counter++
-        monthsTimer = setTimeout(tick, interval) // (*)
+        monthsTimer = setTimeout(tick, interval)
       }, interval)
 
       return
@@ -72,22 +79,31 @@ const Map = () => {
     s2() {
       clearTimeout(monthsTimer)
       clearTimeout(yearTimer)
-      document.getElementById('sc_mar18').classList.add('is-active')
+      Selector.toggleDisabledStatus(true)
+      Selector.setValue('mar')
       document.getElementById('sc_slide2').classList.add('is-active')
       document.getElementById('sc_mar18-av').classList.add('is-active')
-      labelEl.innerHTML = 'March'
-      document.getElementById('sc_dec18').classList.remove('is-active')
+      medianLegendEl.classList.add('is-active')
+      descEl.innerHTML = text['s2']
+
+      document.getElementById('sc_mar17-av').classList.remove('is-active')
+      document.getElementById('sc_mar16-av').classList.remove('is-active')
+      document.getElementById('sc_mar15-av').classList.remove('is-active')
       document.getElementById('sc_slide3').classList.remove('is-active')
+      IceSelector.toggleCheckedStatus(true)
+      IceSelector.toggleVisibility('remove')
+      IceSelector.toggleDisabledStatus(true)
       return
     },
     s3() {
-      document.getElementById('sc_mar18').classList.add('is-active')
-      labelEl.innerHTML = 'March'
+      Selector.setValue('mar')
       document.getElementById('sc_slide2').classList.add('is-active')
       document.getElementById('sc_slide3').classList.remove('is-active')
-      document.getElementById('sc_sep18').classList.remove('is-active')
+      descEl.innerHTML = text['s2']
 
-      const years = ['18', '17', '16', '15']
+      IceSelector.toggleVisibility('add')
+
+      const years = ['17', '16', '15']
 
       let counter = 0
       yearTimer = setTimeout(function tick() {
@@ -97,22 +113,26 @@ const Map = () => {
           .classList.add('is-active')
 
         if (counter === years.length - 1) {
+          descEl.innerHTML = text['s3']
           clearTimeout(yearTimer)
+
+          IceSelector.toggleDisabledStatus(false)
+
           return
         }
 
         counter++
-        yearTimer = setTimeout(tick, interval) // (*)
+        yearTimer = setTimeout(tick, interval)
       }, interval)
       return
     },
     s4() {
       clearTimeout(yearTimer)
       document.getElementById('sc_slide3').classList.add('is-active')
-      document.getElementById('sc_sep18').classList.add('is-active')
-      labelEl.innerHTML = 'September'
-      document.getElementById('sc_mar18').classList.remove('is-active')
+      Selector.setValue('sep')
       document.getElementById('sc_slide2').classList.remove('is-active')
+      IceSelector.toggleVisibility('remove')
+      descEl.innerHTML = text['s4']
     }
   }
 
