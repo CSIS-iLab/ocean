ba42017c7f72a69d544da8df3b409886788efc27
leezaweb <leeza.web@gmail.com>
2019-05-15 16:08:04 -0400

    detect footnotes click in order to expand

--- assets/_js/smooth-scroll.js
+++ assets/_js/smooth-scroll.js
@@ -5,6 +5,24 @@ const smoothScroll = () => {
     header: '.site-header',
     speed: 500
   })
+
+  const expandFootnotes = event => {
+    if (event.detail.toggle.classList.contains('footnote')) {
+      const readMore = document.querySelector('button.read-more')
+      const hiddenFootnotes = Array.from(
+        document.querySelectorAll('.footnotes ol li:nth-child(3) ~ li')
+      )
+      hiddenFootnotes.forEach(footnote => {
+        footnote.style.visibility = 'visible'
+        footnote.style.position = 'static'
+      })
+      readMore.innerText = 'Read Less'
+      readMore.classList.add('is-active')
+      readMore.setAttribute('aria-expanded', 'true')
+    }
+  }
+
+  document.addEventListener('scrollStart', expandFootnotes, false)
 }
 
 export default smoothScroll
