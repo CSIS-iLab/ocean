8049d130e219cf7e3a5414e3995b6114f00ea74b
Mariel de la Garza <mariel.delagarza@viget.com>
2023-04-20 19:04:53 -0400

    update tina variables

--- tina/config.js
+++ tina/config.js
@@ -15,8 +15,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master'
 
 export default defineConfig({
   branch,
-  clientId: 'c00f2ec3-ee4c-4ac7-ba0d-a17edd32cba5', // Get this from tina.io
-  token: 'e793e9a4490db0ed760b025bf78760b953814498', // Get this from tina.io
+  clientId: TINA_CLIENT, // Get this from tina.io
+  token: TINA_TOKEN, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: 'admin',
