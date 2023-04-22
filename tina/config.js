c8af6919a6d540e6a6558e99bbb20f3d70dec03b
jdromero88 <jdromerov88@gmail.com>
2023-04-21 21:48:15 -0400

    Update scripts and config.js

--- tina/config.js
+++ tina/config.js
@@ -11,12 +11,16 @@ import posts from './collections/posts'
 import spotlights from './collections/spotlights'
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master'
+// const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'master'
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+  process.env.HEAD // Netlify branch env
 
 export default defineConfig({
   branch,
-  clientId: 'TINA_CLIENT', // Get this from tina.io
-  token: 'TINA_TOKEN', // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID, // Get this from tina.io
+  token: process.env.TINA_TOKEN, // Get this from tina.io
   client: { skip: true },
   build: {
     outputFolder: 'admin',
